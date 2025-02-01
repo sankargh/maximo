6ed63d23eebed02c68f1ed3dd57bfea2af8cc53b
Sankar Ganesh <sankargvs@sankargvs.com>
2025-02-01 19:45:38 +1100

    Added comments
    Comments added bout script flow

--- Maximo_JMS_Queue_Count.py
+++ Maximo_JMS_Queue_Count.py
@@ -4,13 +4,21 @@ from psdi.iface.jms import QueueConfig
 from psdi.iface.jms import JMSData
 
 qbrowser = None
+
+#Declar, set the Maximo JMS queue name
 queueName = "jms/maximo/int/queues/cqin"
+
+#Set the condition to filter the Messages
 selector = "SENDER='ABC'"
+
+#Connect to JMS queue and Retreive Messages to a List
 config = MaxQueueCache.getInstance().getQueueConfig(queueName);
 connectionFactory = config.getConfactName();
 env = config.getEnv()
 qbrowser = JMSQueueBrowser(queueName, connectionFactory, selector, env);
 qList = qbrowser.getAllMessages();
+
+#Get Message count
 qCount=len(qList)
 
 #raise TypeError("Messages Count - "+str(qCount))
